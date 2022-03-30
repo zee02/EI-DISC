9f7f81df485b3724138490282dea62993cfc24cd
José Fernandes <zefrancisco2002@gmail.com>
2022-03-30 09:20:10 +0100

    ex5 & 6(almost done)

--- ficha3.py
+++ ficha3.py
@@ -11,7 +11,7 @@ B={3,4,5,6,7}
 C={1,5,9,10}
 
 #a
-U=A|B|C # U=A.union(B.union(C))
+U=A|B|C # U=A.union(B.union(C)) União de conjuntos
 print(U)
 
 #b
@@ -34,3 +34,47 @@ AdsB = A^B #diferença simétrica
 
 def simdiff(A,B):
     return (A-B) | (B-A)  # ou (A|B) -(A&B)
+
+#ex5
+#a)
+B=set(range(70,141,7))
+#ou
+B=set()
+for i in range(66, 141):
+    if i%7==0:
+        B.add(i)
+#ou
+B={x for x in range(66,141) if x%7==0}
+
+#b)
+A=set(range(3,101,3))
+C=A^B
+print(len((A|C)&B))
+
+#ex6
+#a
+#teoria de conjuntos e operações de conjuntos
+P=set(range(2,411,2)) #pares
+M7=set(range(7,411,7)) #multiplos de 7
+A=P-M7
+print(len(A))
+
+#b
+for i in range(1, 410):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

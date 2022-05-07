9a1130f604f7b3bc27c5d7cefd3b29ce225cdaca
José Fernandes <zefrancisco2002@gmail.com>
2022-05-07 15:41:22 +0100

    Aula online quarta-feira

--- ficha4.py
+++ ficha4.py
@@ -1,223 +1,230 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr  6 09:57:02 2022
+Created on Wed Apr  6 09:38:02 2022
 
-@author: zeeee
+@author: zclpa
 """
 
-#Ficha 4
-#ex1a)
-import math
-print("São necessários", +str(math.ceil(100/8)) + 'bytes' )
+import matplotlib.pyplot as plt
+import math as mt
 
-#ex1b)
+# %% Exercicio 1 a)
+
+print("São necessários", str(mt.ceil(100 / 8)), "bytes")
+
+# b)
+
+
 def conversor(x):
-    print("São necessários", +str(math.ceil(x/8))+ 'bytes')
-    
+    print("São necessários", str(mt.ceil(x / 8)), "bytes")
 
 
-#ex2
+conversor(100)
+
+
+# %% Exercicio 2
+
+
 def inteiro(x):
-    if x>=0:
-        return math.floor(x)
+    if x >= 0:
+        print("A parte inteiro do número real " + str(x) + " é " + str(mt.floor(x)))
     else:
-        return math.ceil(x)
-    
-#ex3
-#op1 fazendo Leitura do enunciado
+        print("A parte inteiro do número real " + str(x) + " é " + str(mt.ceil(x)))
+
+
+inteiro(-3.6)
+
+# %% Exercicio 3
+
+
 def bissexto(x):
-    if x%4==0:
-        if x%100==0 and x%400!=0:
-            print("Não é bissexto")
-        else:
-            print("é bissexto")
+    if x % 4 == 0 and x % 100 != 0 or x % 400 == 0:
+        print("É ano bissexto")
     else:
-        print("não é bissexto")
-        
-#ou
-def Bissexto(x):
-    if x%4==0 or x%100==0 and x%500!=0:
-            print("Não é bissexto")
-    else:
-        print("é bissexto")
+        print("Não é ano bissexto")
 
-#ex4
-#op1 recursiva chamando a si propria
-def F(n):
-    if n==1 or n==2:
-        return 1
-    elif n>=3:
-        return F(n-1)+F(n-2)
-    else:
-        print("Insira um inteiro positivo")
-        
-F(6)
 
-#op2 definir F(n) sem chamar a si propria
+bissexto(2015)
+
+
+# %% Exercicio 4
+
+
 def F(n):
-    if n==1 or n==2:
+    if n == 1 or n == 2:
         return 1
+    elif n >= 3:
+        return F(n - 1) + F(n - 2)
     else:
-        x=1
-        y=1
-        for i in range(3,n+1):
-            z=x+y
-            x=y
-            y=z
-        return z
-    
-#ex5
-def soma_Fib(n):
-    soma=0
-    for i in range(1,n+1):
-        soma+=F(i)
+        print("Insira um número positivo")
+
+
+# % Exercicio 5
+
+
+def Fib(n):
+    soma = 0
+    for i in range(1, n + 1):
+        soma += F(i)
     return soma
-soma_Fib(6)
-
-#ex7
-import math
-nouro=(1+math.sqrt(5))/2
-n=2
-Q=1
-while abs(Q-nouro)>10**-10:
-    n+=1
-    Q=F(n)/F(n-1)
-print("O valor aproximado obtido é: ", Q)
-#ou
-print(f'O valor aproximado solicitado é (Q) e é atingido quando n={n}')
-
-#b) representação gráfica
-import  matplotlib.pyplot as plt
-
-nouro=(1+math.sqrt(5))/2
-n=2
-Q=1
-X=[2] #iniciar a lista dos objetos começando por n=2
-#como array X=np.array([2])
-Y=[1] #iniciar a lista das imagins com a primeira
-###
-while abs(Q-nouro)>10**-10:
-    n+=1
-    Q=F(n)/F(n-1)
-    X.append(n)
-    Y.append(Q)
-    ###
-plt.plot(X,Y,'*')
-
-#ex10
-
-ns=input("Insira o número de serie da nota: ")
-letra=ns[0]
-numeros=ns[1:len(ns)]
-soma=0
-for i in range(0,len(numeros)):
-    soma+=int(numeros[i])
-letras=['R','S','T', 'U', 'M', 'N']
-Beta=letras.index(letra)
-
-soma+=Beta # beta+x1+x2+..+x10+d
-#a soma e o 0 são congruentes ao modulo 9
-#ou seja soma%9==0%9 logo soma tem de ser multiplo de 9
-
-if soma%9==0:
-    print("A nota é válida!")
-else:
-    print("A nota não é válida!")
-    
-#aula8
-#1A)
-import numpy as np
-MR=np.array([[1,0,1,0], [1,0,1,0], [1,1,0,0], [0,0,0,1]])
-print(MR)
 
 
-#1B) A relação inversa de R^-1{(aj,ai) | (ai,aj) in R}
-MRinv=MR.T
-print("A matriz da relação inversa é: ", MRinv)
-
-#1C)
-MR2 = MR@MR
-
-def um(M):
-    [l,c] = M.shape
-    for i in range(0,l):
-        for j in range(0,c):
-            if M[i,j]!=0:
-                M[i,j] = 1
-    return M
-MR2=um(MR2)
-print("A matriz da relação R^2 é: ", MR2)
-
-#ou
-MR2 = MR@MR
-MR2[MR2!=0]=1
+Y = Fib(6)
+print(Y)
 
 
-MR3=MR2@MR
-MR3[MR3!=0]=1
-print("A matriz da relação R^3 é: ", MR3)
+# %% Exercicio 4 - Alternativo sem função
 
-#1D)
-print("A relação R não é reflexiva porque existem entradas nulas na diagonal principal")
-print("R não é simétrica porque, por ex, (2,1) in R mas (1,2) not in R")
-print("Não é antissimétrica porque (1,3) in R e (3,1) in R mas 1 não é igual a 3")
 
-#1E)
-#fecho reflexivo
-MRefR= MR + np.eye(4)
-MRefR=um(MRefR)
+# def F(n):
+#    if n == 1 or n == 2:
+#        return 1
+#    else:
+#        x = 1
+#        y = 1
+#        for i in range(3, n + 1):
+#            z = x + y
+#            x = y
+#            y = z
+#            return z
 
-#ex4
-def is_reflexiva(M):
-    [l,c]=M.shape
-    contador=0
-    for i in range(0,l):
-        contador+=1
-    if contador!=l:
-        return False
+
+# print(F(6))
+
+
+# %% Exercicio 7
+
+
+nouro = (1 + mt.sqrt(5)) / 2
+
+n = 2
+Q = 1
+
+
+def F(n):
+    if n == 1 or n == 2:
+        return 1
+    elif n >= 3:
+        return F(n - 1) + F(n - 2)
     else:
-        return True
-    
-#ou 
+        print("Insira um número positivo")
 
-def is_reflexiva(M):
-    [l,c]=M.shape
-    for i in range(0,l):
-        if M[i,i] == 0:
-            return False
-    return True
 
-#ex4B)
+while abs(Q - nouro) > 10 ** -10:
+    n += 1
+    Q = F(n) / F(n - 1)
+    plt.plot(n, Q, "*r")
+print("O valor aproximado é ", Q)
 
-def is_simetrica(M):
-    [l,c]=M.shape
-    for i in range(0,l):
-        for j in range(i+1,c):
-            if M[i,j]!=M[j,i]:
-                return False
-        return True
-#ou
-def is_simetrica(M):
-    return np.array_equal(M,M.T)
 
-#ou
-def is_simetrica(M):
-    return (M==M.T).all()
+# %% Exercicio 9
 
-#ex4C)
+# a cong mod m b se a%m = b%m
 
-def is_transitiva(M):
-    A=M+M@M
-    A[A!=0]=1
-    return (M==A).all()
+# d%7 = (50+A+(seculo-1)//4 + A //4-2*(seculo-1))%7
+# como d=0,1,2..6 d%7=d
+# d = (50+A+(seculo-1)//4 + A //4-2*(seculo-1))%7
 
-#ou calculando o fecho transitivo e comparando com MR
 
-def is_transitiva(M):
-    [l,c]=M.shape
-    A=M #A será a matriz do fecho transitivo
-    for i in range(2,l+1):
-        A=A+np.linalg.matrix_power(MR,i)
-    A[A!=0]=1
-    return (M==A).all()
+def Natal(ano):
+    A = ano % 100  # se ano fr str '2017' int(ano[2:3])
+    seculo = mt.ceil(ano / 100)
+    d = (50+A+(seculo-1)//4 + A // 4-2*(seculo-1)) % 7
+    DIAS = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    print(f'No ano {ano} o Natal é à/ao {DIAS[d]}')
+
+
+Natal(2017)
+Natal(2032)
+Natal(2050)
+
+
+# %% Exercicio 10
+ns = input("Insira o nº de série da nota: ")
+letra = ns[0]
+numeros = ns[1: len(ns)]
+
+soma = 0
+
+for i in range(0, len(numeros)):
+    soma += int(numeros[i])
+letras = ["R", "S", "T", "U", "M", "N"]
+Beta = letras.index(letra)
+
+soma += Beta  # beta+x1+x2+...+x10+d
+# a soma e o 0 são congruentes modulo 0, ou seja % 9 == 0 % 9
+# logo a soma tem de ser multiplo de 9
+
+if soma % 9 == 0:
+    print("A nota é válida")
+else:
+    print("A nota não é válida")
+
+
+# %% Exercicio 11 a)
+
+def Identificador():
+    n = input('Insira o número de estudante: ')
+    campus = int(n[0])
+    ano = int(n[1:3])
+    # d4+d5+d6+d7%5 = D%5 = D
+    soma = 0
+    for i in range(3, len(n)):
+        soma += int(n[i])
+
+    D = soma % 5
+    DIAS = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta']
+
+    print(f'Campus {campus} - Ano letivo {ano}/{ano+1} - {DIAS[D]}-feira')
+
+
+Identificador()
+
+# %% Exercicio 12 a)
+
+#CC = input('Insira o número do cartão de crédito: ')
+# 5027811020103X
+
+# 1047712040206X
+
+#34 + X
+
+# para ser multiplo de 10 X tem de ser 6
+
+
+# %% Exercicio 12 b)
+# 4012888888881882
+
+
+def validar():
+    CC = input('Insira o número de cartão de crédito: ')
+    CClist = list(CC)
+    for i in range(0, len(CC)):
+        CClist[i] = int(CClist[i])
+    CClist.reverse()
+    # ou sem usar o comando reverse podemos usar um for e uma variável auxiliar
+    # aux=list()
+    # for i in range(0,len(CC)):
+    #    aux[i]=CClist[len(CC)-1-i]
+    for i in range(1, len(CC), 2):  # range(inicio,limite,step)
+        X = CClist[i]
+        if 2*X > 9:
+            X = int(2*X/10)+(2*X) % 10  # ou X=2*X; X=str(X) ;X=int(X[0])+int(X[1])
+        else:
+            X = 2*X
+        CClist[i] = X
+    soma = 0
+    for i in range(0, len(CC)):
+        soma += CClist[i]
+    if soma % 10 == 0:  # soma tem de ser congruente com 0 modulo 10
+        # soma%10=0%10=0
+        print('O Cartão de crédito é válido')
+    else:
+        print('O Cartão de crédito é inválido')
+
+
+validar()
+# Insira o número de cartão de crédito: 378282246310004
+# O Cartão de crédito é inválido
+# Insira o número de cartão de crédito: 371449635398431
+# O Cartão de crédito é válido
